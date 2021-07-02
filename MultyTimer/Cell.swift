3581e062bab4f9e102def687d99ba3b1e3ac6b0e
imoiseeva <moiseeva.irina@gmail.com>
2021-07-03 02:39:30 +0300

    added constrains

--- MultyTimer/Cell.swift
+++ MultyTimer/Cell.swift
@@ -14,23 +14,10 @@ class Cell: UITableViewCell {
     
     var timer: Model? {
         didSet {
-            
             nameOfTimer.text = timer?.timerName
             time.text = timer?.time
-            
         }
     }
-//
-//
-//    var timerWithSeconds: String? {
-//        didSet {
-//            guard let timerName = timerName else { return }
-//            setupSubvies()
-//            setConstraints()
-//        }
-//    }
-    
-   
     
     private var nameOfTimer: UILabel = {
         let label = UILabel()
@@ -63,16 +50,10 @@ class Cell: UITableViewCell {
             
             nameOfTimer.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: offset),
             nameOfTimer.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            
-           // time.topAnchor.constraint(equalTo: contentView.topAnchor),
+
             time.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -offset),
             time.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
-            //time.heightAnchor.constraint(equalTo: weatherIcon.widthAnchor),
-          //  time.leadingAnchor.constraint(equalTo: nameOfTimer.trailingAnchor, constant: 50),
-           // time.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -offset)
-//            time.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-//            time.leadingAnchor.constraint(equalTo: nameOfTimer.leadingAnchor, constant: -20),
-//            time.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -offset)
+
         ])
     }
     
--- MultyTimer/MainViewController.swift
+++ MultyTimer/MainViewController.swift
@@ -13,22 +13,57 @@ class MainViewController: UIViewController {
     
     var countOfNumbers: [Model] = [Model]()
     
- // var dataFromModel = Model()
+    private var addTimerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Добавление таймеров"
+        label.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        label.font = UIFont.boldSystemFont(ofSize: 10.0)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var nameOfTimer: UITextField = {
+       let textField = UITextField()
+        textField.attributedPlaceholder = NSAttributedString(string: "Название таймера",
+                                                             attributes: [NSAttributedString.Key.font:UIFont(name: "Georgia", size: 10.0)!])
+        
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private var timeInSeconds: UITextField = {
+       let textField = UITextField()
+        textField.placeholder = "Время в секудах"
+        textField.attributedPlaceholder = NSAttributedString(string: "Время в секудах",
+                                                             attributes: [NSAttributedString.Key.font:UIFont(name: "Georgia", size: 10.0)!])
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private var buttonAdd: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = #colorLiteral(red: 0.809979856, green: 0.8100972176, blue: 0.809954226, alpha: 1)
+        button.setTitle("Добавить", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
+       // button.frame.size.width = 400
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(addTimer), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
-        
-        
+     
         setupNavigationBar()
         setupIUElements()
         setConstraints()
-        
-//        if countOfNumbers.isEmpty {
-//            countOfNumbers = Array(repeating: dataFromModel, count: dataFromModel.timerArray.count)
-//        }
-            // tableView.reloadData()
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -60,6 +95,10 @@ class MainViewController: UIViewController {
         tableView.frame = .zero
         
         view.addSubview(tableView)
+        view.addSubview(addTimerLabel)
+        view.addSubview(nameOfTimer)
+        view.addSubview(timeInSeconds)
+        view.addSubview(buttonAdd)
         
         tableView.translatesAutoresizingMaskIntoConstraints = false
     }
@@ -67,11 +106,36 @@ class MainViewController: UIViewController {
     private func setConstraints() {
         
         NSLayoutConstraint.activate([
+            addTimerLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            addTimerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            
+            nameOfTimer.topAnchor.constraint(equalTo: addTimerLabel.bottomAnchor, constant: 10),
+            nameOfTimer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            nameOfTimer.widthAnchor.constraint(equalToConstant: 200),
+            nameOfTimer.heightAnchor.constraint(equalToConstant: 20),
+            
+            
+            timeInSeconds.topAnchor.constraint(equalTo: nameOfTimer.bottomAnchor, constant: 10),
+            timeInSeconds.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            timeInSeconds.widthAnchor.constraint(equalToConstant: 200),
+            timeInSeconds.heightAnchor.constraint(equalToConstant: 20),
+            
+            buttonAdd.topAnchor.constraint(equalTo: timeInSeconds.bottomAnchor, constant: 15),
+            buttonAdd.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 10),
+            buttonAdd.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            //buttonAdd.widthAnchor.constraint(equalToConstant: 250),
+        
+            
+            
             tableView.topAnchor.constraint(equalTo: view.centerYAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+    
+    @objc func addTimer() {
+        
     }
 
 }
